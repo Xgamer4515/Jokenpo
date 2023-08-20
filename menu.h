4c58af3ef33f1c665a78859b524d54fbd28af409
Xgamer4515 <128556541+Xgamer4515@users.noreply.github.com>
2023-08-19 23:38:28 -0300

    menu.h

--- menu.h
+++ menu.h
@@ -1,6 +1,24 @@
 #ifndef MENU_H
 #define MENU_H
 
+#ifdef _WIN32
+#include <Windows.h>
+
+inline void ConfigurarAcentuacaoConsole() {
+    system("chcp 65001 > nul");
+    SetConsoleCP(CP_UTF8);
+    SetConsoleOutputCP(CP_UTF8);
+}
+#endif
+
+inline void LimparTela() {
+#ifdef _WIN32
+    system("cls");
+#else
+    system("clear");
+#endif
+}
+
 void Menu();
 
 #endif // MENU_H
